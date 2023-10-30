5431f022e0c5185824c02a2a17b1c62a6a5f6de3
Simen Smehaugen <simdevgen@outlook.com>
2023-10-30 16:12:55 +0100

    added loader visibility

--- script.js
+++ script.js
@@ -4,11 +4,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Loading function, show loading
+const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+};
+
+// Hide Loading
+const complete = () => {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show new quote function
 const newQuote = () => {
+    loading();
     // Random quote from APIQUOTES array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace it with 'unknown'
@@ -23,12 +37,14 @@ const newQuote = () => {
     } else {
         quoteText.classList.remove('long-quote');
     }
-    // Add the actual quote itself
+// Set Quote, Hide loader
     quoteText.textContent = quote.text;
+    complete();
 };
 
 // Get quotes from API
 const getQuotes = async () => {
+    loading();
     const APIURL = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     try {
         // Wont be populated until we have the data from the API url
